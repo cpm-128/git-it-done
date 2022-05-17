4cc384481d0c4545f61a9a04974836af0d976ac1
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-17 11:11:03 -0400

    added functionality to display issues on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -42,17 +42,32 @@ var displayRepos = function(repos, searchTerm) {
 
         // format repo name
         var repoName = repos[i].owner.login + "/" + repos[i].name;
+        
+            // create a container for each repo
+            var repoEl = document.createElement("div");
+            repoEl.classList = "list-item flex-row justify-space-between align-center";
 
-        // create a container for each repo
-        var repoEl = document.createElement("div");
-        repoEl.classList = "list-item flex-row justify-space-between align-center";
+            // create a span element to hold repository name
+            var titleEl = document.createElement("span");
+            titleEl.textContent = repoName;
 
-        // create a span element to hold repository name
-        var titleEl = document.createElement("span");
-        titleEl.textContent = repoName;
+            // append to container
+            repoEl.appendChild(titleEl);
+            // this completes creating the title of the repo
 
-        // append to container
-        repoEl.appendChild(titleEl);
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+            // check if current repo has issues or not and create HTML for inside the Element
+            if (repos[i].open_issues_count > 0) {
+                statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+            } else {
+                statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+            }
+
+            // append to container
+            repoEl.appendChild(statusEl);
 
         // append container to the DOM
         repoContainerEl.appendChild(repoEl);

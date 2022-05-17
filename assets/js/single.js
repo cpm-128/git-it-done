75b1744e8f8e9e110e53383afce8f09b202a544a
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-17 13:45:11 -0400

    created and tested fetch api

---
+++ assets/js/single.js
@@ -0,0 +1,16 @@
+var getRepoIssues = function(repo) {
+    console.log(repo);
+    var apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiURL)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                });
+            } else {
+                alert("There was a problem with the request.");
+            }
+        });
+};
+
+getRepoIssues("facebook/react");

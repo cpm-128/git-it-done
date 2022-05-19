938e9bb5243f1b25a0eb89cec6d0d49801ad8514
cpm-128 <cpm-128@users.noreply.github.com>
2022-05-18 21:05:37 -0400

    ability to search by language and populate the page with issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -107,4 +108,16 @@ var getFeaturedRepos = function(language) {
         });
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    console.log(">>> language >>>" , event.target.getAttribute("data-language"));
+    if(language) {
+        getFeaturedRepos(language);
+
+        //clear old content in repo container
+        repoContainerEl.textContent="";
+    }
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click" , buttonClickHandler);
